85294ac7742b27d22c432c78daa8163c553b633a
natelloyd1 <nathanaellloyd@live.com>
2020-03-22 15:51:11 +0000

    add files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = ({}) => (
+<>  
+    <div className="bg">
+        <header className="pingHeader jumbotron mt-4 mb-0">
+            <h1 className="headerH1">5 Day Workout Plan</h1>
+        </header>
+        </div> 
+</>
+);
+
+export default App;
