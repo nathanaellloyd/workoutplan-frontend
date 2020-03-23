1833cfe4449f16afccf8423aba2150753d1a83f1
natelloyd1 <nathanaellloyd@live.com>
2020-03-23 14:26:30 +0000

    card template

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const header = ({}) => (
+<>  
+        <div className="bg">
+        <header className="pingHeader jumbotron mt-4 mb-0">
+            <h1 className="headerH1">Corona Cardio Routine</h1>
+        </header>
+        </div>
+        <card1 />  
+</>
+);
+
+export default header;
