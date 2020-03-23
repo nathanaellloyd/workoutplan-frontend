57350b8ea14093e3f8aee60540e241d32c482e5c
natelloyd1 <nathanaellloyd@live.com>
2020-03-23 15:24:01 +0000

    moved images

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Card1= () =>  (
+const Card1 = () =>  (
 <div className="col-md-6 mt-4">
     <div className="card text-center">
         <h5 className="card-header"></h5>
         <div className="card-body">
-        <img src="./images/card1.png" alt="example img"/>
+        <img src="./images/card1" alt="example img"/>
         <p className="card-text display-1">Day 1</p>
         </div>
         <div className="card-footer">
