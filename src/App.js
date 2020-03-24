2f059daa23b7c061a443c890db6c4fcafaa3ce22
JordanBelmont <jordan.belmont@hotmail.com>
2020-03-24 12:47:45 +0000

    testing out transitions on cards and adding custom fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Card from "../Cards/Card"
-import Header from "../Header";
+import Card from "./components/Cards/Card";
+import CardTest from "./components/Cards/CardTest";
+import Header from "./components/Header";
 
 const App = ({}) => (
 <>  
         <Header />
+        <CardTest />
         <Card title="Day 1" />
         <Card title="Day 2" />
         <Card title="Day 3" />
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 // import './index.css';
-import App from './components/App/App';
+import App from './App';
 import store from './store';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ store }>
