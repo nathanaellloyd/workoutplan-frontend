b9d93f9d7392ad44f708913073a252734da8df9e
natelloyd1 <nathanaellloyd@live.com>
2020-03-26 21:52:35 +0000

    error page

--- src/Error.js
+++ src/Error.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import bacteria from "./images/bacteria.jpg";
 
-const Error = () => <p>Nope</p>;
+
+const Error = () => (
+    <>
+        <div className="PNFwrap">
+            <img alt="bacteria" className="bacteria" src={bacteria} />
+            <p className="pageNotFoundTitle">Page Not Found.</p>
+            <p className="pageNotFoundText">If you believe this happened in error, please <a href="/">contact us</a></p>
+        </div>
+    </>
+); 
 
 export default Error;
