ad3a1fbdfc956520e258a1431f2860b28d2e726a
JordanBelmont <jordan.belmont@hotmail.com>
2020-03-27 12:20:19 +0000

    more arbitrary error changes

--- src/Error.js
+++ src/Error.js
@@ -7,7 +7,7 @@ const Error = () => (
     <>
         <div className="PNFwrap">
             <img alt="Jerry" className="jerry" src={seinfeld} />
-            <p className="pageNotFoundTitle">"What's the deal with all these errors?!"</p>
+            <p className="pageNotFoundTitle">"What's the <i>deal</i> with all these errors?!"</p>
             <p className="pageNotFoundText">If you believe this happened in error, please <a href="/">contact us</a></p>
         </div>
     </>
