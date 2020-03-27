f0d1d3b09b43d6c6a292c3c1c85309dad8cbb238
JordanBelmont <jordan.belmont@hotmail.com>
2020-03-27 09:23:11 +0000

    Updates

---
+++ precache-manifest.e780d57b52191c896b6e43b06dcadb7d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed71fb056f3b10cd1fced64297df0d8a",
+    "url": "/workoutplan-frontend/index.html"
+  },
+  {
+    "revision": "9919dcb3041de6e38360",
+    "url": "/workoutplan-frontend/static/css/main.5f145c88.chunk.css"
+  },
+  {
+    "revision": "9a8736c6042dcb498a7a",
+    "url": "/workoutplan-frontend/static/js/2.abd6fcaa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/workoutplan-frontend/static/js/2.abd6fcaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9919dcb3041de6e38360",
+    "url": "/workoutplan-frontend/static/js/main.67e17542.chunk.js"
+  },
+  {
+    "revision": "fd22b21cb3c261c4f01f",
+    "url": "/workoutplan-frontend/static/js/runtime-main.c53b7dee.js"
+  },
+  {
+    "revision": "32c768d384586a4f3fd4264cd4097f67",
+    "url": "/workoutplan-frontend/static/media/Coronaviral.32c768d3.otf"
+  },
+  {
+    "revision": "83f0931bc7f1f6148e2757ec2a91b2fa",
+    "url": "/workoutplan-frontend/static/media/Evogria.83f0931b.otf"
+  },
+  {
+    "revision": "f20f653bcc60af751294c8deed242c6e",
+    "url": "/workoutplan-frontend/static/media/EvogriaItalic.f20f653b.otf"
+  },
+  {
+    "revision": "35ae702d95bc967034a2a9c15568e726",
+    "url": "/workoutplan-frontend/static/media/Mont-ExtraLight.35ae702d.otf"
+  },
+  {
+    "revision": "7d90a574d6ef3b7949d79a56b4d201db",
+    "url": "/workoutplan-frontend/static/media/Mont-Heavy.7d90a574.otf"
+  },
+  {
+    "revision": "b265b78cb1f414ca0fe9008ef1ffbef7",
+    "url": "/workoutplan-frontend/static/media/bacteria.b265b78c.jpg"
+  },
+  {
+    "revision": "6ca3ed0fea0b89a6c15f18aeeb5c6ead",
+    "url": "/workoutplan-frontend/static/media/instagram2.6ca3ed0f.png"
+  },
+  {
+    "revision": "31f4cbbb7d818d22bd180ac8c9c5f66e",
+    "url": "/workoutplan-frontend/static/media/linkedin-in2.31f4cbbb.png"
+  },
+  {
+    "revision": "c8f7c0549dbbb50685cef2d691d0772d",
+    "url": "/workoutplan-frontend/static/media/twitter2.c8f7c054.png"
+  },
+  {
+    "revision": "519a1027a54f09ba86c115d350712309",
+    "url": "/workoutplan-frontend/static/media/viral.519a1027.png"
+  },
+  {
+    "revision": "ad5117110ad9752203a5030a7ce93b4c",
+    "url": "/workoutplan-frontend/static/media/youtube2.ad511711.png"
+  }
+]);
