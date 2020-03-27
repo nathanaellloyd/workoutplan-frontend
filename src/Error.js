474668c24aab4971044ff7686eaa0d8e2c588c9b
JordanBelmont <jordan.belmont@hotmail.com>
2020-03-27 12:14:45 +0000

    new jerry

--- src/Error.js
+++ src/Error.js
@@ -6,7 +6,7 @@ import seinfeld from "./images/seinfeld.jpg";
 const Error = () => (
     <>
         <div className="PNFwrap">
-            <img alt="bacteria" className="bacteria" src={seinfeld} />
+            <img alt="Jerry" className="jerry" src={seinfeld} />
             <p className="pageNotFoundTitle">"What's the deal with all these errors?!"</p>
             <p className="pageNotFoundText">If you believe this happened in error, please <a href="/">contact us</a></p>
         </div>
